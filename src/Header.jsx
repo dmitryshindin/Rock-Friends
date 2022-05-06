d3f0d88931967cefb52a320546f979b75a6b4421
DimaEvchuk <74135637+DimaEvchuk@users.noreply.github.com>
2022-05-06 22:54:04 +0300

    Header 1

---
+++ src/Header.jsx
@@ -0,0 +1,40 @@
+function Header() {
+    return(
+        <header id="header">
+        <div className="header">
+            <nav>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="#header" id="Rock_Friends">RockFriends</a>
+                        </li>
+                        
+                        <li>
+                            <a href="" id="Menu">Menu</a>
+                        </li>
+                        
+                        <li>
+                            <a href="" id="Galeria">Galeria</a>
+                        </li>
+                        <li>
+                            <a href="" id="Kontakt">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="inner">
+                <h2>
+                    <strong>Rock<br/>Friends</strong>
+                </h2>
+                <p>
+                Rock Friends — это атмосфера рок-н-ролла в самом сердце Варшавы.
+                <br/>
+                Широкий выбор алкоголя и коктейлей, легкие закуски и много хорошей музыки.
+                </p>
+            </div>
+        </div>
+        </header>
+    )
+}
+
+export default Header;
