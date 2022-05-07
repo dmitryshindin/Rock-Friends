84b46ed787dde0b4fe018ff4f4bfe6df277db8f9
DimaEvchuk <74135637+DimaEvchuk@users.noreply.github.com>
2022-05-07 14:47:04 +0300

    delete header

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import Header from './Header';
 function App() {
   return (
     <div>
-      <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      </ul>
       <Header />
       <Galeria />
     </div>
