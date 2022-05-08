e30cbe4da8fea7716721215dfdb5e69aa10f9701
DimaEvchuk <74135637+DimaEvchuk@users.noreply.github.com>
2022-05-08 13:58:14 +0300

    Galeria update

--- src/Galeria/Galeria.jsx
+++ src/Galeria/Galeria.jsx
@@ -1,6 +1,42 @@
 import "./Galeria.css";
 
 function Galeria() {
+    
+
+    
+  const allPicturesName = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+  ];
+
+  const _allPictures = allPicturesName.map(
+    (_name) => "/assets/Galeria-foto/" + _name
+  );
+
+  //  console.log(_allPictures)
+
+  const Pictures = _allPictures.map(function (name) {
+    return (
+      <div className="grid-box">
+        <a href={name} data-lightbox="galeria">
+          <img src={name} alt="" />
+        </a>
+      </div>
+    );
+  });
+
   return (
     <section id="galeria">
       <div>
@@ -9,13 +45,7 @@ function Galeria() {
           <h1 className="text_bold">GALERIA</h1>
           <hr />
         </div>
-        <div className="picture-grid">
-          <div className="grid-box">
-            <a href="" data-lightbox="galeria">
-              <img src="/assets/Galeria-foto/1.jpg" alt="" />
-            </a>
-          </div>
-        </div>
+        <div className="picture-grid">{Pictures}</div>
       </div>
     </section>
   );
